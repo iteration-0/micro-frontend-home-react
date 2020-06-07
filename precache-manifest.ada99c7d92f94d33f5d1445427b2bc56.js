79151994e06fd4e7c3e84690d37d50615f0ea800
sunwei <sunwei@users.noreply.github.com>
2020-06-07 02:11:02 +0000

    deploy: 0fa6a60ed4649c44c842820bc0c9b565cfb09993

---
+++ precache-manifest.ada99c7d92f94d33f5d1445427b2bc56.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0f6ee2039b39c5e1917707c9656c00d5",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b2f1eb66cbb3235c124f",
+    "url": "/static/css/main.a75eed77.css"
+  },
+  {
+    "revision": "b2f1eb66cbb3235c124f",
+    "url": "/static/js/main.6913c67f.js"
+  },
+  {
+    "revision": "d4b9583852996254d9a5b908c50798f8",
+    "url": "/static/js/main.6913c67f.js.LICENSE.txt"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.aeff4d77feea9f4d18ef224d0dd4ef43.js"
+  "/precache-manifest.ada99c7d92f94d33f5d1445427b2bc56.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -35,5 +35,5 @@ workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
 workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
-  blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
+  blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
